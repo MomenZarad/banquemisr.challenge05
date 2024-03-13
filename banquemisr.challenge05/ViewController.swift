7165a1f8d986634ddb0ec93822ce1a05749ba661
Momen <momen_zarad@yahoo.com>
2024-03-14 00:55:30 +0200

    initial commit

---
+++ banquemisr.challenge05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  banquemisr.challenge05
+//
+//  Created by Momen Zarad on 14/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
